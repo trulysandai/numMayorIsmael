282e6bf8c228a980a257396579cf806e82ae27a7
Ismael Chacón <124751246+trulysandai@users.noreply.github.com>
2023-04-26 20:05:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var numero01;
+var numero02;
+numero01=parseInt(prompt("Ingrese el numero 1",""));
+numero02=parseInt(prompt("Ingrese el numero 2",""));
+if(numero01 >= numero02)
+{
+  //imprime numero mayor
+	document.write("El numero mayor es: " + numero01 + " ( Numero 1 )");
+}
+else
+{
+  //imprime numero mayor
+	document.write("El Numero mayor es : " + numero02 + " ( Numero 2 )");
+}
